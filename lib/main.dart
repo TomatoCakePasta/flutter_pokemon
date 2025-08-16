9433a4d87e92d9fd25bdbc25110e15741a5d08cc
TomatoCakePasta <gaomuk50@gmail.com>
2025-08-16 13:41:58 +0900

    実装:詳細ページのUI

--- lib/main.dart
+++ lib/main.dart
@@ -29,14 +29,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Hello World'),
+          children: [
+            Stack(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(32),
+                  child:  Image.network(
+                    url,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text(
+                    "No.25",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    )
+                  )
+                )
+              ],
+            ),
+            const Text(
+              "pikachu",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Chip(
+              label: Text("electric"),
+              backgroundColor: Colors.yellow,
+            ),
           ],
         ),
       ),
